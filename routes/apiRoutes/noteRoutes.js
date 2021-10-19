44a2c54916d1461a96dc854851a4261938b3ca8f
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-10-18 20:32:16 -0500

    good

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { filterByQuery, findById, createNewAnimal, validateAnimal, } = require("../../lib/animals");
-const { animals } = require("../../data/animals");
+const { notes } = require("../../data/notes");
 
 router.get("/animals", (req, res) => {
   let results = animals;
