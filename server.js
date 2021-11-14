80b1d3531c3375be9c78e6672aa5c5b4ef1ba3ee
kaohzina <zinanshin.kaoher@gmail.com>
2021-11-14 13:27:20 -0600

    server.js work

---
+++ server.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/routes')(app);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
