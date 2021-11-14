1c8f60122b86a41dc23bf1e295c163c022ff81c7
kaohzina <zinanshin.kaoher@gmail.com>
2021-11-14 15:08:36 -0600

    more working progress..

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,34 +1,21 @@
 const router = require('express').Router();
-let db = require('../db/db.json')
-const fs = require('fs')
+let db = require('../../db/db.json');
 
-console.log("look here", uuidv4());
-router.get("/notes", (req, res) => {
-    console.log("here we are");
-    // let results = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    res.json(db);
+router.get('/notes', (req, res) => {
+  res.json(db);
 });
 
-router.post("/notes", (req, res) => {
-    
-    req.body.id =uuidv4();
-    const newNote = req.body;
-    // let notesData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    db.push(newNote);
-    // fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
-    res.json(db);
-})
 
-router.delete("/notes/:id", (req, res) => {
-    let noteID = req.params.id.toString();
-    // let noteData = JSON.parse(
-    //     fs.readFileSync( "./db/db.json", "utf8"));
-    
-    
-    const newData =db.filter(note => note.id != noteID);
-    console.log('we got halfway', newData)
-    // fs.writeFileSync('./db/db.json', JSON.stringify(newData));
-    db = newData;
-    res.json(db);
+router.post('/notes', (req, res) => {
+  req.body.id = notes.length.toString();
+
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    const note = newNote(req.body, notes);
+    db.push(newNote)
+    res.json(note);
+  }
 });
+
 module.exports = router;
